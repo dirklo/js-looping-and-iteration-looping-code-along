37fa91ae331f9409021f236293137006caaa38d5
Rick <dirklo@gmail.com>
2021-03-15 14:25:45 +0100

    commit tree cf92a6b445f017b522301f20310775103c5379fd

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function writeCards(names) {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`)
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
